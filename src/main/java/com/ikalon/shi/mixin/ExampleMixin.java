c295de799995fa637f584589428ebba1ea95acf7
system-controller <systemcontroller@protonmail.com>
2022-06-05 16:27:17 -0400

    Set up the project structure.

--- src/main/java/com/ikalon/shi/mixin/ExampleMixin.java
+++ src/main/java/com/ikalon/shi/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.ikalon.shi.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.ikalon.shi.SHI;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		SHI.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
